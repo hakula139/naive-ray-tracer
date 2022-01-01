4eb6f8444eea9b940efcc3852e1350bc675ef7b6
Hakula Chen <i@hakula.xyz>
2022-01-02 05:22:54 +0800

    feat: implement shadows
    refactor: modify variable names

--- src/tracer.cpp
+++ src/tracer.cpp
@@ -11,25 +11,27 @@
 #include "sphere.hpp"
 
 using geo::Vec3;
+using std::vector;
 
 Vec3 Reflect(const Vec3& i, const Vec3& n) { return i - n * 2.f * (i * n); }
 
 bool SceneIntersect(
     const Vec3& origin,
-    const Vec3& direction,
-    const std::vector<Sphere>& spheres,
-    Vec3* p_hit,
-    Vec3* p_n,
-    Material* p_material) {
+    const Vec3& dir,
+    const vector<Sphere>& spheres,
+    Vec3* p_hit = nullptr,
+    Vec3* p_n = nullptr,
+    Material* p_material = nullptr) {
   auto spheres_dist = std::numeric_limits<float>::max();
   for (const auto& sphere : spheres) {
     auto sphere_dist_i = std::numeric_limits<float>::max();
-    if (RaySphereIntersect(origin, direction, sphere, &sphere_dist_i) &&
+    if (RaySphereIntersect(origin, dir, sphere, &sphere_dist_i) &&
         sphere_dist_i < spheres_dist) {
       spheres_dist = sphere_dist_i;
-      *p_hit = origin + direction * sphere_dist_i;
-      *p_n = (*p_hit - sphere.center).normalize();
-      *p_material = sphere.material;
+      auto hit = origin + dir * sphere_dist_i;
+      if (p_hit) *p_hit = hit;
+      if (p_n) *p_n = (hit - sphere.center).normalize();
+      if (p_material) *p_material = sphere.material;
     }
   }
   return spheres_dist < MAX_DIST;
@@ -37,14 +39,14 @@ bool SceneIntersect(
 
 Vec3 CastRay(
     const Vec3& origin,
-    const Vec3& direction,
-    const std::vector<Sphere>& spheres,
-    const std::vector<Light>& lights) {
-  Material material;
+    const Vec3& dir,
+    const vector<Sphere>& spheres,
+    const vector<Light>& lights) {
   Vec3 hit;
   Vec3 n;
+  Material material;
 
-  if (!SceneIntersect(origin, direction, spheres, &hit, &n, &material)) {
+  if (!SceneIntersect(origin, dir, spheres, &hit, &n, &material)) {
     return Vec3 {0.2, 0.7, 0.8};  // background color
   }
 
@@ -52,12 +54,19 @@ Vec3 CastRay(
   auto specular_light_intensity = 0.f;
 
   for (const auto& light : lights) {
-    auto light_direction = (light.position - hit).normalize();
-    auto diffuse_rate = std::max(0.f, light_direction * n);
+    auto light_dir = (light.position - hit).normalize();
+    Vec3 shadow_hit;
+
+    if (SceneIntersect(hit, light_dir, spheres, &shadow_hit) &&
+        (shadow_hit - hit).norm() < (light.position - hit).norm()) {
+      continue;
+    }
+
+    auto diffuse_rate = std::max(0.f, light_dir * n);
     diffuse_light_intensity += light.intensity * diffuse_rate;
+
     auto specular_rate = powf(
-        std::max(0.f, Reflect(light_direction, n) * direction),
-        material.specular_exponent);
+        std::max(0.f, Reflect(light_dir, n) * dir), material.specular_exponent);
     specular_light_intensity += light.intensity * specular_rate;
   }
 
@@ -65,17 +74,16 @@ Vec3 CastRay(
          Vec3 {1, 1, 1} * specular_light_intensity * material.albedo[1];
 }
 
-void Render(
-    const std::vector<Sphere>& spheres, const std::vector<Light>& lights) {
-  std::vector<Vec3> frame_buf(WIDTH * HEIGHT);
+void Render(const vector<Sphere>& spheres, const vector<Light>& lights) {
+  vector<Vec3> frame_buf(WIDTH * HEIGHT);
 
   for (size_t j = 0; j < HEIGHT; ++j) {
     for (size_t i = 0; i < WIDTH; ++i) {
       auto x = (2 * (i + 0.5f) / WIDTH - 1) * tanf(FOV / 2.f) * WIDTH / HEIGHT;
       auto y = -(2 * (j + 0.5f) / HEIGHT - 1) * tanf(FOV / 2.f);
       auto origin = Vec3 {0, 0, 0};
-      auto direction = Vec3 {x, y, -1}.normalize();
-      frame_buf[i + j * WIDTH] = CastRay(origin, direction, spheres, lights);
+      auto dir = Vec3 {x, y, -1}.normalize();
+      frame_buf[i + j * WIDTH] = CastRay(origin, dir, spheres, lights);
     }
   }
 
@@ -95,14 +103,14 @@ int main() {
   const Material RED_RUBBER {1.0, {0.9, 0.1, 0.0, 0.0}, {0.3, 0.1, 0.1}, 10.};
   const Material MIRROR {1.0, {0.0, 10.0, 0.8, 0.0}, {1.0, 1.0, 1.0}, 1425.};
 
-  std::vector<Sphere> spheres {
+  vector<Sphere> spheres {
       Sphere {Vec3 {-3, 0, -16}, 2, IVORY},
       Sphere {Vec3 {-1.0, -1.5, -12}, 2, GLASS},
       Sphere {Vec3 {1.5, -0.5, -18}, 3, RED_RUBBER},
       Sphere {Vec3 {7, 5, -18}, 4, MIRROR},
   };
 
-  std::vector<Light> lights {
+  vector<Light> lights {
       {{-20, 20, 20}, 1.5},
       {{30, 50, -25}, 1.8},
       {{30, 20, 30}, 1.7},
